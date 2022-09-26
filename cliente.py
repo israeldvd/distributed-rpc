9127753addae104d7b202010cf6293baeac0aadf
Israel David <israelcaetano21@hotmail.com>
2022-09-26 15:29:20 -0300

    Define código-base para cliente e servidor
    - Cria arquivo do servidor
    - Edita cliente.py: Dá maior complexidade

--- cliente.py
+++ cliente.py
@@ -1,19 +1,34 @@
-# Este exemplo foi retirado desta doc.: https://tldp.org/HOWTO/XML-RPC-HOWTO/xmlrpc-howto-python.html
-
+import sys
 import xmlrpc.client
 
+# Função para validar entrada
+
+
+def validar_entrada_input(texto_string, interruption_key='q'):
+    if texto_string == interruption_key:
+        print("Saindo...")
+        sys.exit(-1)
+
+    entrada = input(texto_string)
+
+    if entrada.isdigit():
+        return int(entrada)
+    else:
+        entrada = input('Digite um valor inteiro válido\n')
+        return validar_entrada_input(entrada)
+
+
 # Cria um objeto para representar nosso servidor
-url_servidor = 'http://xmlrpc-c.sourceforge.net/api/sample.php'
+url_servidor = 'http://localhost:8000'
 servidor = xmlrpc.client.ServerProxy(url_servidor)
-print(servidor)  # Objeto ServerProxy
 
 # Inspeção dos métodos existentes
 print("Métodos:", servidor.system.listMethods(), end="\n\n")
 
 # Faz chamada ao servidor e obtém nosso resultado
-resultado = servidor.sample.sumAndDifference(
-    5, 3)
-print("Resultado (objeto):", resultado)  # Dicionário
+while True:
+    a = validar_entrada_input("Digite um valor inteiro ou 'q' para sair:\n")
+    b = validar_entrada_input("Digite o segundo valor:\n")
 
-print("Soma:", resultado['sum'])  # 8
-print("Diferença:", resultado['difference'])  # 2
+    print("Resultado:", servidor.mdc(a, b))  # Dicionário
+    print()
---
+++ servidor_mdc.py
@@ -0,0 +1,28 @@
+import sys
+from xmlrpc.server import SimpleXMLRPCServer
+
+PORT = 8000
+
+# Define a função MDC entre dois números -- modo recursivo
+
+
+def mdc(a, b):
+    return a if not b else mdc(b, a % b)
+
+
+# Registra o servidor com gerenciador de contexto
+with SimpleXMLRPCServer(('localhost', PORT)) as servidor:
+    print(f"Servidor ativado na porta {PORT}...\nUse Ctrl + C para deixar")
+
+    # Registra as funções de inspeção system.listMethods, system.methodHelp and system.methodSignature
+    servidor.register_introspection_functions()
+
+    # Registra o método de MDC -- usa o mesmo nome para chamada da função
+    servidor.register_function(mdc)
+
+    # Deixa o servidor em loop
+    try:
+        servidor.serve_forever()
+    except KeyboardInterrupt:
+        print("\nInterrupção feita pelo teclado. Finalizando...")
+        sys.exit(0)
