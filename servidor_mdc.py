3ce00ae8aadd8a78ae836da16232521456c9d051
Israel David <israelcaetano21@hotmail.com>
2022-09-27 12:01:35 -0300

    Executa MDC mediante chamadas indiretas
    - Define outras mudanças como posição de print()
    - Traduz termo em língua estrangeira
    - Calcula o tempo decorrido de operação
    - Generaliza função que calcula para chamada indireta

--- servidor_mdc.py
+++ servidor_mdc.py
@@ -1,29 +1,48 @@
 import sys
+from datetime import timedelta, time
+from timeit import default_timer as timer
 from xmlrpc.server import SimpleXMLRPCServer
 
 PORT = 8000
 
+# Calcula tempo decorrido
+
+def exec_temporizada(func, *args):
+    tempo_inicial = timer()
+    return (func(*args), timer() - tempo_inicial)
+
 # Define a função MDC entre dois números -- modo recursivo
 
-
 def mdc(a, b):
-    '''Retorna o MDC entre dois números inteiros -- independente do sinal'''
-    return a if not b else mdc(b, a % b)
+    '''Retorna o MDC entre dois números inteiros, sejam eles positivos ou negativos'''
+
+    @staticmethod
+    def calculos(x, y):
+        while y:
+            x, y = y, x % y
+        return x
+
+    tupla_ret = exec_temporizada(calculos, a, b)
+    print(f'\tmdc({a}, {b}): {tupla_ret[0]}')
+    print(f'\ttempo decorrido: {timedelta(seconds=tupla_ret[1])}')
+    print()
+    return tupla_ret[0]
 
 
 # Registra o servidor com gerenciador de contexto
 with SimpleXMLRPCServer(('localhost', PORT)) as servidor:
-    print(f"Servidor ativado na porta {PORT}...\nUse Ctrl + C para deixar")
-
-    # Registra as funções de inspeção system.listMethods, system.methodHelp and system.methodSignature
+    # Registra as funções de inspeção system.listMethods, system.methodHelp e system.methodSignature
     servidor.register_introspection_functions()
 
     # Registra o método de MDC -- usa o mesmo nome para chamada da função
     servidor.register_function(mdc)
 
     # Deixa o servidor em loop
+    print(f"Servidor ativado na porta {PORT}...\nUse Ctrl + C para deixar")
     try:
         servidor.serve_forever()
     except KeyboardInterrupt:
         print("\nInterrupção feita pelo teclado. Finalizando...")
         sys.exit(0)
+
+
